e8053ce836bd9ef53b91f012e34e7918f15695d4
pappzsolt0606 <71691840+pappzsolt0606@users.noreply.github.com>
2022-03-11 14:56:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+3. Feladat
+Írj egy programot while ciklussal, amely kiírja a páratlan számokat csökkenő sorrendben 1 és 10 között!
+'''
+szam = 11
+
+while szam > 0:
+    if szam == 11:
+        pass
+    else:
+        print(szam)
+    szam -= 2
